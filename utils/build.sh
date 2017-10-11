422987527ea81f29bdcce069f6770b2cd0b79596
Cuiwenchang <756661204@qq.com>
2017-10-11 18:15:06 +0800

    update

--- utils/build.sh
+++ utils/build.sh
@@ -1,1 +1,1 @@
-pyinstaller bash_container.py --onefile -F --hidden-import docker --path ./venn/lib/python2.7/site-packages -n bash-app
+pyinstaller bash_container.py --onefile -F --hidden-import docker --path ./venn/lib/python2.7/site-packages -n bash-appM
