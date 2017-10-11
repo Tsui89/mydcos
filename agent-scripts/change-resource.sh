6f9ded0bc57a92526eaf00a1fdd0ffd5f4128039
Cuiwenchang <756661204@qq.com>
2017-10-11 18:21:01 +0800

    update

--- agent-scripts/change-resource.sh
+++ agent-scripts/change-resource.sh
@@ -1,4 +1,4 @@
-#$ scp utils/make_disk_resources.py root@192.168.131.3:/opt/mesosphere/bin/make_disk_resources.py
+cp ../utils/make_disk_resources.py /opt/mesosphere/bin/make_disk_resources.py
 
 rm -f /var/lib/dcos/mesos-resources
 rm -f /var/lib/mesos/slave/meta/slaves/latest
